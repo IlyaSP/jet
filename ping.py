9ad97615aea71bb3a4397f4161f2f35ea13fb74b
Ilya <37418897+IlyaSP@users.noreply.github.com>
2021-01-20 17:01:14 +0300

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,12 @@
+import os
+import datetime
+import time
+
+
+ping = os.system("ping -c  3 8.8.8.8")
+now = datetime.datetime.now().strftime('%d %b %H:%M')
+if ping == 0:
+	print("last status is OK {0}\n".format(now))
+else:
+	print("last status is FAILD {0}\n".format(now))
+		ilya@ilya-docker:~$
